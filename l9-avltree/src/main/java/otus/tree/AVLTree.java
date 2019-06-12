e3617777237a8034e41112817c65b70224fbd3a4
maxdudin <dudin@phystech.edu>
2019-06-12 23:51:19 +0300

    avl tree implementation covered with tests remove method is in progress

--- l9-avltree/src/main/java/otus/tree/AVLTree.java
+++ l9-avltree/src/main/java/otus/tree/AVLTree.java
@@ -1,19 +1,30 @@
 package otus.tree;
 
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class AVLTree<K extends Comparable<K>, V> {
     private TreeNode root;
 
-    private class TreeNode {
+    public class TreeNode {
+        TreeNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
         K key;
-        V node;
+        V value;
         TreeNode parent;
-        TreeNode left;
-        TreeNode right;
-        List<TreeNode> equalElements;
-        int balanceFactor;
-        int height;
+        TreeNode left = null;
+        TreeNode right = null;
+        List<TreeNode> equalElements = new ArrayList<TreeNode>();
+        int balanceFactor = 0;
+        int height = 0;
+        boolean deleted = false;
     }
 
     /**
@@ -22,77 +33,38 @@ public class AVLTree<K extends Comparable<K>, V> {
      * 2. Updates nodes properties
      * 3. Does tree rebalancing
      * 4. Returns new tree root node
+     *
      * @param newNode
      * @return
      */
-    public TreeNode insert(TreeNode newNode) {
+    public void insert(TreeNode newNode) {
         if (root == null) {
             root = newNode;
-//            dostuff;
+            return;
         }
 
-        return insert(root, newNode);
+        insert(root, newNode);
     }
 
     private TreeNode insert(TreeNode subTree, TreeNode node) {
+        if (subTree == null) {
+            return node;
+        }
 
         if (node.key.compareTo(subTree.key) < 0) {
-
-            if (node.left == null) {
-
-            } else {
-                node.left = insert(node.left, node);
-            }
-
-
+            subTree.left = insert(subTree.left, node);
+            subTree.left.parent = subTree;
         } else if (node.key.compareTo(subTree.key) > 0) {
-
-            if (node.right == null) {
-
-            } else {
-                node.right = insert(node.right, node);
-            }
-
+            subTree.right = insert(subTree.right, node);
+            subTree.right.parent = subTree;
         } else {
             subTree.equalElements.add(node);
+
             return subTree;
         }
 
-        updateHeight(node);
-        return balance(node);
-    }
-
-    public void findPlaceToInsert(TreeNode newNode) {
-//        TreeNode iterNode = root;
-//
-//        while (iterNode != null) {
-//            if (newNode.key.compareTo(iterNode.key) < 0) {
-//                if (iterNode.left == null) {
-//                    newNode.parent = iterNode;
-//                    newNode.height = 0;
-//                    newNode.balanceFactor = 0;
-//                    iterNode.left = newNode;
-//                    height
-//                    return;
-//                }
-//
-//                iterNode = iterNode.left;
-//            } else if (newNode.key.compareTo(iterNode.key) > 0) {
-//                if (iterNode.right == null) {
-//                    newNode.parent = iterNode;
-//                    newNode.height = 0;
-//                    newNode.balanceFactor = 0;
-//                    iterNode.right = newNode;
-//                    height
-//                    return;
-//                }
-//
-//                iterNode = iterNode.right;
-//            } else {
-//                iterNode.equalElements.add(newNode);
-//                return;
-//            }
-//        }
+        updateHeight(subTree);
+        return balance(subTree);
     }
 
     private void updateHeight(TreeNode node) {
@@ -131,18 +103,155 @@ public class AVLTree<K extends Comparable<K>, V> {
     }
 
     private TreeNode leftRightCase(TreeNode node) {
-
+        node.left = leftRotation(node.left);
+        return leftLeftCase(node);
     }
 
     private TreeNode rightLeftCase(TreeNode node) {
-
+        node.right = rightRotation(node.right);
+        return rightRightCase(node);
     }
 
+    //incoming node means A node from picture
+    //B is a left child of A
     private TreeNode rightRotation(TreeNode node) {
+        TreeNode nodeA = node;
+        TreeNode nodeB = node.left;
+        TreeNode nodeP = node.parent;
+        nodeA.left = nodeB.right;
 
+        if (nodeB.right != null) {
+            nodeB.right.parent = nodeA;
+        }
+
+        nodeB.right = nodeA;
+        nodeA.parent = nodeB;
+        nodeB.parent = nodeP;
+        if (nodeP != null) {
+            if (nodeP.right != null && nodeP.right.key == nodeA.key) {
+                nodeP.right = nodeB;
+            }
+            if (nodeP.left != null && nodeP.left.key == nodeA.key) {
+                nodeP.left = nodeB;
+            }
+        } else {
+            root = nodeB;
+        }
+
+        updateHeight(nodeA);
+        updateHeight(nodeB);
+
+        return nodeB;
     }
+
+    /**
+     * removes an element with a given key from subtree
+     * @param subTree
+     * @param rmKey
+     * @return a successor or deleted node if successor is not required
+     */
+    private TreeNode remove(TreeNode subTree, K rmKey) {
+        if (subTree == null) {
+            return null;
+        }
+
+        if (rmKey.compareTo(subTree.key) < 0) {
+            TreeNode newLeftNode = remove(subTree.left, rmKey);
+            if (newLeftNode == null) return null;
+            if (newLeftNode.deleted) return newLeftNode;
+            subTree.left = newLeftNode;
+
+        } else if (rmKey.compareTo(subTree.key) > 0) {
+            TreeNode newRightNode = remove(subTree.right, rmKey);
+            if (newRightNode == null) return null;
+            if (newRightNode.deleted) return newRightNode;
+            subTree.right = newRightNode;
+
+        } else {
+            if (subTree.left == null) {
+                return subTree.right;
+
+            } else if (subTree.right == null) {
+                return subTree.left;
+
+            } else {
+                if (!subTree.equalElements.isEmpty()) {
+                    TreeNode del = subTree.equalElements.remove(0);
+                    del.deleted = true;
+                    return del;
+                }
+
+                TreeNode cur = subTree.left;
+                while (cur.right != null) {
+                    cur = cur.right;
+                }
+                TreeNode successor = cur;
+                subTree.key = successor.key;
+                subTree.value = successor.value;
+
+                TreeNode replacement = remove(subTree.left, subTree.key);
+                subTree.left = replacement;
+            }
+        }
+
+        updateHeight(subTree);
+        return balance(subTree);
+    }
+    //incoming node means A node from picture
+    //B is a right child of A
 
     private TreeNode leftRotation(TreeNode node) {
+        TreeNode nodeA = node;
+        TreeNode nodeB = node.right;
+        TreeNode nodeP = node.parent;
+        nodeA.right = nodeB.left;
 
+        if (nodeB.left != null) {
+            nodeB.left.parent = nodeA;
+        }
+
+        nodeB.left = nodeA;
+        nodeA.parent = nodeB;
+        nodeB.parent = nodeP;
+        if (nodeP != null) {
+            if (nodeP.right != null && nodeP.right.key == nodeA.key) {
+                nodeP.right = nodeB;
+            }
+            if (nodeP.left != null && nodeP.left.key == nodeA.key) {
+                nodeP.left = nodeB;
+            }
+        } else {
+            root = nodeB;
+        }
+
+        updateHeight(nodeA);
+        updateHeight(nodeB);
+
+        return nodeB;
+    }
+
+    public List<K> getBFSRepresentation() {
+        List<K> repr = new ArrayList<>();
+
+        if (root == null) {
+            return repr;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+
+            repr.add(cur.key);
+        }
+
+        return repr;
     }
 }
--- l9-avltree/src/test/java/otus/tree/TestAVLTree.java
+++ l9-avltree/src/test/java/otus/tree/TestAVLTree.java
@@ -1,4 +1,98 @@
 package otus.tree;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestAVLTree {
+    @Test
+    public void simpleAVLTreeCreationTest() {
+        AVLTree<Integer, Integer> avlTree = new AVLTree<>();
+
+        AVLTree<Integer, Integer>.TreeNode one = avlTree.new TreeNode(1, 1);
+        avlTree.insert(one);
+        List<Integer> expectedReprOne = List.of(1);
+        List<Integer> actualReprOne = avlTree.getBFSRepresentation();
+        assertEquals(expectedReprOne, actualReprOne);
+
+
+        AVLTree<Integer, Integer>.TreeNode five = avlTree.new TreeNode(5, 5);
+        avlTree.insert(five);
+        List<Integer> expectedReprFive = List.of(1, 5);
+        List<Integer> actualReprFive = avlTree.getBFSRepresentation();
+        assertEquals(expectedReprFive, actualReprFive);
+
+
+        AVLTree<Integer, Integer>.TreeNode four = avlTree.new TreeNode(4, 4);
+        avlTree.insert(four);
+        List<Integer> expectedReprFour = List.of(4, 1, 5);
+        List<Integer> actualReprFour = avlTree.getBFSRepresentation();
+        assertEquals(expectedReprFour, actualReprFour);
+
+
+        AVLTree<Integer, Integer>.TreeNode two = avlTree.new TreeNode(2, 2);
+        avlTree.insert(two);
+        List<Integer> expectedReprTwo = List.of(4, 1, 5, 2);
+        List<Integer> actualReprTwo = avlTree.getBFSRepresentation();
+        assertEquals(actualReprTwo, expectedReprTwo);
+
+
+        AVLTree<Integer, Integer>.TreeNode three = avlTree.new TreeNode(3, 3);
+        avlTree.insert(three);
+        List<Integer> expectedReprThree = List.of(4, 2, 5, 1, 3);
+        List<Integer> actualReprThree = avlTree.getBFSRepresentation();
+        assertEquals(actualReprThree, expectedReprThree);
+
+
+        AVLTree<Integer, Integer>.TreeNode six = avlTree.new TreeNode(6, 6);
+        avlTree.insert(six);
+        List<Integer> expectedReprSix = List.of(4, 2, 5, 1, 3, 6);
+        List<Integer> actualReprSix = avlTree.getBFSRepresentation();
+        assertEquals(actualReprSix, expectedReprSix);
+
+
+        AVLTree<Integer, Integer>.TreeNode nine = avlTree.new TreeNode(9, 9);
+        avlTree.insert(nine);
+        List<Integer> expectedReprNine = List.of(4, 2, 6, 1, 3, 5, 9);
+        List<Integer> actualReprNine = avlTree.getBFSRepresentation();
+        assertEquals(actualReprNine, expectedReprNine);
+
+
+        AVLTree<Integer, Integer>.TreeNode eight = avlTree.new TreeNode(8, 8);
+        avlTree.insert(eight);
+        List<Integer> expectedReprEight = List.of(4, 2, 6, 1, 3, 5, 9, 8);
+        List<Integer> actualReprEight = avlTree.getBFSRepresentation();
+        assertEquals(actualReprEight, expectedReprEight);
+
+
+        AVLTree<Integer, Integer>.TreeNode seven = avlTree.new TreeNode(7, 7);
+        avlTree.insert(seven);
+        List<Integer> expectedReprSeven = List.of(4, 2, 6, 1, 3, 5, 8, 7, 9);
+        List<Integer> actualReprSeven = avlTree.getBFSRepresentation();
+        assertEquals(actualReprSeven, expectedReprSeven);
+
+
+        AVLTree<Integer, Integer>.TreeNode ten = avlTree.new TreeNode(10, 10);
+        avlTree.insert(ten);
+        List<Integer> expectedReprTen = List.of(4, 2, 8, 1, 3, 6, 9, 5, 7, 10);
+        List<Integer> actualReprTen = avlTree.getBFSRepresentation();
+        assertEquals(actualReprTen, expectedReprTen);
+
+
+        AVLTree<Integer, Integer>.TreeNode eleven = avlTree.new TreeNode(11, 11);
+        avlTree.insert(eleven);
+        List<Integer> expectedReprEleven = List.of(4, 2, 8, 1, 3, 6, 10, 5, 7, 9, 11);
+        List<Integer> actualReprEleven = avlTree.getBFSRepresentation();
+        assertEquals(actualReprEleven, expectedReprEleven);
+
+
+        AVLTree<Integer, Integer>.TreeNode twelve = avlTree.new TreeNode(12, 12);
+        avlTree.insert(twelve);
+        List<Integer> expectedReprTwelve = List.of(8, 4, 10, 2, 6, 9, 11, 1, 3, 5, 7, 12);
+        List<Integer> actualReprTwelve = avlTree.getBFSRepresentation();
+        assertEquals(actualReprTwelve, expectedReprTwelve);
+    }
+
 }
